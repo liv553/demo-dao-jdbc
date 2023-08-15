9f13d7fc2875fd13e3bdebd1a69a14aa79aafd94
liv553 <livia.balmeida@ufpe.br>
2023-08-15 00:04:39 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -39,5 +39,10 @@ public class Program {
         System.out.println("Inserted! New id = " + newSeller.getId());
 
 
+        System.out.println("\n=== TEST 5: seller update ====");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Maine");
+        sellerDao.update(seller);
+        System.out.println("Update completed");
     }
 }
